ceb8038bd22f99ae9365b2085fdcfe4b7dce457c
gurmeet1011 <156289823+gurmeet1011@users.noreply.github.com>
2024-10-18 14:14:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ function firstWord(s) {
   // your code here
 	let s2=s.trim();
 	let index=s2.indexOf(' ');
-	let word=s2.slice(0,index+1);
+	if(index==-1)
+		return s;
+	let word=s2.slice(0,index);
 	return word;
 }
 
-// Do not change the code below
+// Do not change the code be
 
 const s = prompt("Enter String:");
 alert(firstWord(s));
